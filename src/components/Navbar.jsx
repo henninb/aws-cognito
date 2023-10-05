08c74631ab0d626f0a985ff68502bb8dfe669758
Brian Henning <henninb@msn.com>
2023-10-05 17:46:22 -0500

    wip

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,69 +1,63 @@
-import React, { Component } from 'react'
-import { Auth } from "aws-amplify";
+import React from 'react';
+import { Auth } from 'aws-amplify';
 
-export default class Navbar extends Component {
-  //onClick={handleLogOut} 
-  handleLogOut = async event => {
+export default function Navbar(props) {
+  const handleLogOut = async (event) => {
     event.preventDefault();
-    
     try {
       Auth.signOut();
-      this.props.auth.setAuthenticatedStatus(false);
-       
-    } catch(error) {
-      console.log(error.message)
+      props.auth.setAuthenticatedStatus(false);
+    } catch (error) {
+      console.log(error.message);
     }
+  };
 
-  }
+  return (
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="/">
+          <img src="hexal-logo.png" width="112" height="28" alt="hexal logo" />
+        </a>
+      </div>
 
-  render() {
-    return (
-      <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="/">
-            <img src="hexal-logo.png" width="112" height="28" alt="hexal logo" />
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+          <a href="/" className="navbar-item">
+            Home
+          </a>
+          <a href="/products" className="navbar-item">
+            Products
+          </a>
+          <a href="/admin" className="navbar-item">
+            Admin
           </a>
         </div>
 
-        <div id="navbarBasicExample" className="navbar-menu">
-          <div className="navbar-start">
-            <a href="/" className="navbar-item">
-              Home
-            </a>
-            <a href="/products" className="navbar-item">
-              Products
-            </a>
-            <a href="/admin" className="navbar-item">
-              Admin
-            </a>
-          </div>
-
-          <div className="navbar-end">
-            <div className="navbar-item">
-              {this.props.auth.isAuthenticated && (
-                <div>
-                <a href="/" className="button is-light">
+        <div className="navbar-end">
+          <div className="navbar-item">
+            {props.auth.isAuthenticated && (
+              <div>
+                <a href="/" className="button is-light" onClick={handleLogOut}>
                   Log out
                 </a>
+              </div>
+            )}
+            <div className="buttons">
+              {!props.auth.isAuthenticated && (
+                <div>
+                  <a href="/register" className="button is-primary">
+                    <strong>Register</strong>
+                  </a>
+                  <a href="/login" className="button is-light">
+                    Log in
+                  </a>
                 </div>
               )}
-              <div className="buttons">
-                {!this.props.auth.isAuthenticated && (
-                  <div>
-                   <a href="/register" className="button is-primary">
-                  <strong>Register</strong>
-                </a>
-                <a href="/login" className="button is-light">
-                  Log in
-                </a>
-                   </div>
-                )}
-
-              </div>
             </div>
           </div>
         </div>
-      </nav>
-    )
-  }
+      </div>
+    </nav>
+  );
 }
+
